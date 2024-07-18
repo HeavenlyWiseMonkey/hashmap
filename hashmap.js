2aed2718a3f3c4489f8cba95e8bfcbe3222672b2
odin <139291798+minecrafter021345@users.noreply.github.com>
2024-07-18 07:37:13 -0600

    Finish hashmap

---
+++ hashmap.js
@@ -0,0 +1,167 @@
+import {Node, LinkedList} from './linked-list.js';
+
+class HashMap {
+    constructor(capacity=16) {
+        this.capacity = capacity;
+        this.map = [];
+        this.loadFactor = 0.75;
+        this.length = 0;
+    }
+    hash(key) {
+        let hashCode = 0;
+        
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+     
+        return hashCode % this.capacity;
+    }
+    set(key, value) {
+        const hashCode = this.hash(key);
+        // check if bucket exists before making a bucket
+        if (!this.map[hashCode]) {
+            this.map[hashCode] = new LinkedList(new Node([key, value]));
+        }
+        // find the key in the bucket to overwrite or add if unique key
+        else {
+            let bucket = this.map[hashCode];
+            let point = bucket.head;
+            while (point) {
+                if (point[0] === key) {
+                    point[1] = value;
+                    return;
+                }
+                point = point.next;
+            }
+            bucket.append([key, value]);
+        }
+        this.grow();
+        this.length++;
+    }
+    get(key) {
+        const hashCode = this.hash(key);
+        let bucket = this.map[hashCode];
+        let point = (bucket) ? bucket.head: null;
+        // console.log(bucket);
+        while (point) {
+            if (point.data[0] === key) {
+                return point.data[1];
+            }
+            point = point.next;
+        }
+        return null;
+    }
+    has(key) {
+        const hashCode = this.hash(key);
+        let bucket = this.map[hashCode];
+        let point = (bucket) ? bucket.head : null;
+        while (point) {
+            if (point.data[0] === key) {
+                return true;
+            }
+            point = point.next;
+        }
+        return false;
+    }
+    remove(key) {
+        const hashCode = this.hash(key);
+        let bucket = this.map[hashCode];
+        let point = bucket.head;
+        let index = 0;
+        const pair = [key, this.get(key)];
+        while (point) {
+            if (pair[0] === point.data[0] && pair[1] === point.data[1]) {
+                bucket.removeAt(index);
+                return true;
+            }
+            index += 1;
+            point = point.next;
+        }
+        return false;
+    }
+    clear() {
+        this.map = [];
+        this.length = 0;
+        this.capacity = 16;
+    }
+    keys() {
+        let keyArray = [];
+        let bucket;
+        let point;
+        let key;
+        for (let i=0; i<this.map.length; i++) {
+            bucket = this.map[i];
+            point = (bucket) ? bucket.head: null;
+            while (point) {
+                key = point.data[0];
+                keyArray.push(key);
+                point = point.next;
+            }
+        }
+        return keyArray;
+    }
+    values() {
+        let valueArray = [];
+        let bucket;
+        let point;
+        let value;
+        for (let i=0; i<this.map.length; i++) {
+            bucket = this.map[i];
+            point = (bucket) ? bucket.head: null;
+            while (point) {
+                value = point.data[1];
+                valueArray.push(value);
+                point = point.next;
+            }
+        }
+        return valueArray;
+    }
+    entries() {
+        let entryArray = [];
+        let bucket;
+        let point;
+        for (let i=0; i<this.map.length; i++) {
+            bucket = this.map[i];
+            point = (bucket) ? bucket.head: null;
+            while (point) {
+                entryArray.push(point.data);
+                point = point.next;
+            }
+        }
+        return entryArray;
+    }
+    grow() {
+        let newArray = [];
+        let bucket;
+        let point;
+        let hashCode;
+        let key;
+        let value;
+        let newArrayBucket;
+        let newArrayPoint;
+        if (this.length >= this.capacity * this.loadFactor) {
+            this.capacity *= 2;
+            for (let i=0; i<this.map.length; i++) {
+                bucket = this.map[i];
+                point = (bucket) ? bucket.head: null;
+                while (point) {
+                    key = point.data[0];
+                    value = point.data[1];
+                    hashCode = this.hash(key);
+                    if (!newArray[hashCode]) {
+                        newArray[hashCode] = new LinkedList(new Node([key, value]));
+                    }
+                    else {
+                        newArrayBucket = newArray[hashCode];
+                        newArrayBucket.append([key, value]);
+                    }
+                    point = point.next;
+                }
+            }
+        }
+        this.map = newArray;
+    }
+}
+
+export default HashMap;
---
+++ linked-list.js
@@ -0,0 +1,154 @@
+class Node {
+    constructor(data=null, next=null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor(head=null) {
+        this.head = head;
+        let point = head;
+        while (point !== null && point.next) {
+            point = point.next;
+        }
+        this.tail = point;
+        this.size = (head) ? 1 : 0;
+    }
+    append(value) {
+        if (!this.head) {
+            this.prepend(value);
+            return;
+        }
+        let point = this.tail;
+        const node = new Node(value);
+        point.next = node;
+        this.tail = point.next;
+        this.size ++;
+    }
+    prepend(value) {
+        let point = this.head;
+        const node = new Node(value);
+        node.next = point;
+        this.head = node;
+        if (!point) {
+            this.tail = node;
+        }
+        this.size ++;
+    }
+    getSize() {
+        return this.size;
+    }
+    getHead() {
+        return this.head;
+    }
+    getTail() {
+        return this.tail;
+    }
+    at(index) {
+        let point = this.head;
+        while (point) {
+            if (index===0) {
+                return point;
+            }
+            point = point.next;
+            index--;
+        }
+        return null;
+    }
+    pop() {
+        let point = this.head;
+        while (point.next!==this.tail) {
+            point = point.next
+        }
+        this.tail = point;
+        this.size --;
+    }
+    contains(value) {
+        let point = this.head;
+        while (point) {
+            if (point.data===value) {
+                return true;
+            }
+            point = point.next;
+        }
+        return false;
+    }
+    find(value) {
+        let point = this.head;
+        let index = 0;
+        while (point) {
+            if (point.data===value) {
+                return index;
+            }
+            index += 1;
+            point = point.next;
+        }
+        return null;
+    }
+    toString() {
+        let point = this.head;
+        let listString = '';
+        while (point) {
+            listString = listString.concat(`( ${point.data} ) -> `);
+            point = point.next;
+        }
+        listString = listString.concat('null');
+        return listString;
+    }
+    insertAt(value, index) {
+        let point = this.head;
+        let node = new Node(value);
+        if (index===0) {
+            this.head = node;
+            this.head.next = point;
+            return;
+        }
+        else if (index===this.size) {
+            point = this.tail;
+            this.tail = node;
+            this.point.next = this.tail;
+            return;
+        }
+        else if (index>this.size) {
+            return;
+        }
+        while (point) {
+            if (index===1) {
+                node.next = point.next;
+                point.next = node;
+            }
+            point = point.next;
+            index--;
+        }
+        this.size ++;
+    }
+    removeAt(index) {
+        let point = this.head;
+        if (index===0) {
+            this.head = this.head.next;
+            return;
+        }
+        else if (index>=this.size) {
+            return;
+        }
+        while (point) {
+            if (index===1) {
+                if (point.next === this.tail) {
+                    this.tail = point;
+                }
+                point.next = point.next.next;
+            }
+            point = point.next;
+            index--;
+        }
+        this.size --;
+    }
+}
+
+// let list = new LinkedList(new Node('apple'));
+// list.append('banana');
+// list.append('cat');
+// console.log(list);
+
+export {Node, LinkedList};
